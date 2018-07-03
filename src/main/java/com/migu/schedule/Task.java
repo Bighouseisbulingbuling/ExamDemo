d1f8539889c0b1c47191817d6a755274d21bca9b
li <13163023559@139.com>
2018-07-03 18:18:45 +0800

    fff

---
+++ src/main/java/com/migu/schedule/Task.java
@@ -0,0 +1,31 @@
+package com.migu.schedule;
+
+public class Task {
+	
+	private Integer taskId;
+	private Integer consumption;
+	
+	public Task(){}
+	public Task(int taskId, int consumption) {
+		this.taskId = taskId;
+		this.consumption = consumption;
+	}
+	
+	public Integer getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(Integer taskId) {
+		this.taskId = taskId;
+	}
+	public Integer getConsumption() {
+		return consumption;
+	}
+	public void setConsumption(Integer consumption) {
+		this.consumption = consumption;
+	}
+	@Override
+	public String toString() {
+		return "Task [taskId=" + taskId + ", consumption=" + consumption + "]";
+	}
+	
+}
